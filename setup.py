5389a858ffa63123854c128c0a92ce4489517edd
Álvaro <alvaro@mbp.local>
2015-03-04 15:27:36 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='branch_client',
+      packages=['branch_client'],
+      version='0.0.1',
+      description='A Python Branch.io client',
+      author='Alvaro Medina Ballester',
+      author_email='amedina@apsl.net',
+      url='https://hg.apsl.net/APSL/branch_client',
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent',
+                   'Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Topic :: Office/Business']
+      )
